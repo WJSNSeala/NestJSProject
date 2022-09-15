c571529404f394d4b6cdf5b6715f9a6b1ddce3bd
WJSNSeala <harhyom9189@gmail.com>
2022-09-15 15:00:34 +0900

    Auth Guard tuto

---
+++ hello-nestjs/src/auth/auth.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    return this.validateRequest(request);
+  }
+
+  private validateRequest(request: any) {
+    return true;
+  }
+}
