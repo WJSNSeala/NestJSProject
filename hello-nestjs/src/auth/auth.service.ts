9698f221a6e73ba436a1ac8721e4d204bc870c53
WJSNSeala <harhyom9189@gmail.com>
2022-09-16 14:55:33 +0900

    create : auth : auth service and config

---
+++ hello-nestjs/src/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import * as jwt from 'jsonwebtoken';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import authConfig from 'src/config/authConfig';
+import { ConfigType } from '@nestjs/config';
+import { Config } from 'prettier';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(authConfig.KEY) private config: ConfigType<typeof authConfig>,
+  ) {}
+
+  login (user: User) {
+    const payload = {...user};
+
+    return jwt.sign(payload, this.config.jwtSecret, {
+      
+    }), 
+  }
+}
