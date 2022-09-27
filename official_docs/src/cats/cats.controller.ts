e86254d2a3389c236ec4d1a4477b5ecc2ae62bb0
WJSNSeala <harhyom9189@gmail.com>
2022-09-27 19:08:30 +0900

    official docs : pipe

--- official_docs/src/cats/cats.controller.ts
+++ official_docs/src/cats/cats.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+  UseFilters,
+  Param,
+  ParseIntPipe,
+  UsePipes,
+} from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './cats.interface';
+import { ForbiddenException } from 'src/exception/forbidden.exception';
+import { HttpExceptionFilter } from 'src/exception/http-exception.filter';
+import { JoiValidationPipe } from 'src/pipes/joi-schema-validation.pipe';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  @UsePipes(new JoiValidationPipe(createCatSchema))
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get('/exception-filters')
+  @UseFilters(new HttpExceptionFilter())
+  async findAllExcept() {
+    throw new ForbiddenException();
+  }
+
+  @Get(':id')
+  async findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    id: number,
+  ) {
+    return this.catsService.findOne(id);
+  }
+}
